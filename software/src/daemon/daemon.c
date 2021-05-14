768005f7fa0f358ff1c70282e6124940f877c66d
Daniel Karling <daniel.karling@gmail.com>
2021-05-14 19:24:26 +0200

    Tweak speed again

--- software/src/daemon/daemon.c
+++ software/src/daemon/daemon.c
@@ -96,7 +96,7 @@ int main(int argc, char *argv[])
 
     while(!done)
     {
-        for(int i = 0; i < 200; i++)
+        for(int i = 0; i < 100; i++)
         {
             i8080_cycle(&cpu);
         }
